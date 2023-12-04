e2e6426fe849304ea8f7200b7af4a7ed38c095d1
br3nr <repetitive_routine@protonmail.com>
2023-12-04 20:36:39 +0800

    Day 04: Scratchcards ★★

---
+++ 04/star_2.py
@@ -0,0 +1,28 @@
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+    file_len = len(lines)
+    match_map = {key: 1 for key in range(1, file_len + 1)}
+    scratch_cards = 0
+    for i, line in enumerate(lines):
+        scratch_cards += match_map[i + 1]
+        for j in range(match_map[i + 1]):
+            winnings_map = {}
+            format = line.replace("  ", " ")
+            format = format[format.rfind(":") + 1 : :]
+            game = format.split("|")
+
+            winnings = game[0].strip().split(" ")
+            numbers = game[1].strip().split(" ")
+
+            for winning in winnings:
+                winnings_map[winning] = 1
+
+            matches = 0
+            for number in numbers:
+                if number in winnings_map:
+                    matches = matches + 1
+            if not matches == 0:
+                for match in range(matches):
+                    match_map[match + i + 2] = match_map.get(i + 2 + match, 0) + 1
+
+print(scratch_cards)
