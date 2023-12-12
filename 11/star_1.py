0aa1d424809ce37d6bb3a07080a930e77d2fb9dd
br3nr <repetitive_routine@protonmail.com>
2023-12-12 19:52:11 +0800

    Day 11: Cosmic Expansion ★

--- 11/star_1.py
+++ 11/star_1.py
@@ -1,17 +1,53 @@
-D = open("test.txt").read().strip()
-lines = D.split("\n")
+def cosmically_inflate(arr):
+    space = []
+    for line in arr:
+        if len(set(line)) == 1:
+            space.append(line)
+        space.append(line)
+    return space
 
-coords = []
-i_iter = 0
-j_iter = 0
-for i, line in enumerate(lines):
-    line_ar = list(line)
-    if(len(set(line_ar))) == 1:
-       i_iter += 2
-    else:
-        for j, char in enumerate(line):
-            if char == "#":
+def rotate_space(arr):
+    space = [list(row) for row in zip(*arr)][::-1]
+    return space
+
+def reset_space(arr):
+    space = [list(row)[::-1] for row in zip(*arr)]
+    return space
+
+def manhatten(x1, y1, x2, y2):
+    return abs(x2 - x1) + abs(y2 - y1)
+
+if __name__ == "__main__":
+    D = open("input.txt").read().strip()
+    lines = D.split("\n")
+    space, coords = [], []
+    
+    for line in lines:
+        space.append(list(line))
+
+    space = rotate_space(cosmically_inflate(space))
+    space = reset_space(cosmically_inflate(space))
+
+    for i in range(len(space)):
+        for j in range(len(space[0])):
+            if space[i][j] == "#":
                 coords.append([i,j])
-        i_iter+=1
 
-print(coords)
+    galaxy_route = []
+    paths = []
+    counter = 1
+    for i, s_galaxy in enumerate(coords):
+        star_jump = 999
+        for j in range(counter, len(coords)):
+            e_galaxy = coords[j]
+            if s_galaxy != e_galaxy:
+                distance = manhatten(s_galaxy[1], s_galaxy[0], e_galaxy[1], e_galaxy[0])
+                paths.append(distance)
+                if distance < star_jump:
+                    star_jump = distance
+        
+        counter += 1
+        galaxy_route.append(star_jump)
+    
+    print(len(galaxy_route))
+    print(sum(paths))
