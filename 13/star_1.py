ecbd3d9bb46ca03011d82001a86da65768b9fc4d
br3nr <repetitive_routine@protonmail.com>
2023-12-14 16:00:45 +0800

    Day 13: Point of Incidence ★

--- 13/star_1.py
+++ 13/star_1.py
@@ -1,18 +1,20 @@
-D = open("test.txt").read().strip()
-lines = D.split("\n\n")
 
-patterns = []
+def get_patterns():
+    D = open("input.txt").read().strip()
+    lines = D.split("\n\n")
+    patterns = []
 
-for pattern in lines:
-    rows = pattern.split("\n")
-    l = [list(x) for x in rows]
-    patterns.append(l)
+    for pattern in lines:
+        rows = pattern.split("\n")
+        l = [list(x) for x in rows]
+        patterns.append(l)
 
 
 def check_mask_symmetry(mask):
     for i in range(len(mask)-1, -1, -1):
         sub_mask = mask[:i+1]
-        if sub_mask == sub_mask[::-1] and len(sub_mask) != 1:
+        if sub_mask == sub_mask[::-1] and len(sub_mask) != 1 and len(sub_mask) % 2 == 0:
+        
             return True
     return False
 
@@ -22,6 +24,7 @@ def get_mask(pattern, map):
     for p in pattern:
         mask.append(map[''.join(p)])
     return mask
+
 
 def map_symmetry(pattern):
     map = create_map(pattern)
@@ -34,12 +37,12 @@ def map_symmetry(pattern):
 
 
 def create_map(pattern):
-    
     map = {}
     for p in pattern:
         p = ''.join(p)
-        map[p] = map.get(p, 0) + 1
+        map[p] = map.get(p, len(map))
     return map
+
 
 def get_contiguous_symmetry(pattern):
     map = create_map(pattern)
@@ -51,14 +54,17 @@ def get_contiguous_symmetry(pattern):
     
     for i in range(len(mask)-1, -1, -1):
         sub_mask = mask[:i+1] 
-        if sub_mask == sub_mask[::-1] and len(sub_mask) != 1:
+        print(sub_mask)
+        if sub_mask == sub_mask[::-1] and len(sub_mask) != 1 and len(sub_mask) % 2 == 0:
             return sub_mask
     
 
 def loop_pattern(pattern):
+    for line in pattern:
+        print(''.join(line))
+
     transposed = False
     if not map_symmetry(pattern):
-        print("needs flip")
         transposed = True
         pattern = [list(row)[::-1] for row in zip(*pattern)]
 
@@ -68,16 +74,26 @@ def loop_pattern(pattern):
         mask.append(map[''.join(p)])
    
     contig = get_contiguous_symmetry(pattern) 
-    
+
     val = len(contig)//2
     for i in range(len(mask) - len(contig) + 1):
         if mask[i:i + len(contig)] == contig:
             val += i
             break
-    
-    print(val)
 
-for pattern in patterns:
-    mid = len(pattern)//2
-    loop_pattern(pattern)
+    return val * 100 if not transposed else val
+
+
+def main():
+    pattern = get_patterns()
+    symetries = 0
+    
+    for i,pattern in enumerate(patterns):
+        symetries += loop_pattern(pattern)
+
+    print(symetries)
+
+
+if __name__ == "__main__":
+    main()
 
